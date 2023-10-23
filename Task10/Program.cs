d4cb6a0b095051000976986acf00d77309962a3d
Viktoria <viky.ne2@gmail.com>
2023-10-23 20:36:33 +0300

    seminar 2

---
+++ Task10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int number2;
+int number=Convert.ToInt32(Console.ReadLine());
+if (number>99 && number<1000)
+{
+    number2=number/10;
+    number=number2%10;
+    Console.WriteLine($"{number}");
+}
+else 
+{
+    Console.WriteLine($"Число {number} не является трехзначным");
+}
+
---
+++ Task13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int number=Convert.ToInt32(Console.ReadLine());
+while (number>999)
+{
+    number=number/10;
+}
+if (number>99 && number<1000)
+{
+    number=number%10;
+    Console.WriteLine($"{number}");
+}
+else 
+{
+    Console.WriteLine("третьей цифры нет");
+}
